ae4602498fd236ccbc2a34b7b035bc2504bb129d
roryclear <roryclear.rc@gmail.com>
2026-02-03 13:54:42 +0000

    move more

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from rfdetr.main import populate_args
 from rfdetr.models.backbone import build_backbone
 from rfdetr.models.transformer import build_transformer
-from rfdetr.models.lwdetr import LWDETR
+from rfdetr.util.misc import NestedTensor, nested_tensor_from_tensor_list
 
 import torch
 from torch import nn
@@ -14,8 +14,11 @@ from typing import List, Literal, Optional, Union
 from copy import deepcopy
 from pydantic import BaseModel, field_validator
 import os
-import torchvision.transforms.functional as F
+import torchvision.transforms.functional as vF
+import torch.nn.functional as F
 from tqdm import tqdm
+import math
+import copy
 
 COCO_CLASSES = {1: "person", 2: "bicycle", 3: "car", 4: "motorcycle", 5: "airplane", 6: "bus", 7: "train", 8: "truck", 9: "boat",
 10: "traffic light", 11: "fire hydrant", 13: "stop sign", 14: "parking meter", 15: "bench", 16: "bird", 17: "cat", 18: "dog",
@@ -60,6 +63,263 @@ OPEN_SOURCE_MODELS = {
 
 
 HOSTED_MODELS = {**OPEN_SOURCE_MODELS, **PLATFORM_MODELS}
+
+class MLP(nn.Module):
+    """ Very simple multi-layer perceptron (also called FFN)"""
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+class LWDETR(nn.Module):
+    """ This is the Group DETR v3 module that performs object detection """
+    def __init__(self,
+                 backbone,
+                 transformer,
+                 segmentation_head,
+                 num_classes,
+                 num_queries,
+                 aux_loss=False,
+                 group_detr=1,
+                 two_stage=False,
+                 lite_refpoint_refine=False,
+                 bbox_reparam=False):
+        """ Initializes the model.
+        Parameters:
+            backbone: torch module of the backbone to be used. See backbone.py
+            transformer: torch module of the transformer architecture. See transformer.py
+            num_classes: number of object classes
+            num_queries: number of object queries, ie detection slot. This is the maximal number of objects
+                         Conditional DETR can detect in a single image. For COCO, we recommend 100 queries.
+            aux_loss: True if auxiliary decoding losses (loss at each decoder layer) are to be used.
+            group_detr: Number of groups to speed detr training. Default is 1.
+            lite_refpoint_refine: TODO
+        """
+        super().__init__()
+        self.num_queries = num_queries
+        self.transformer = transformer
+        hidden_dim = transformer.d_model
+        self.class_embed = nn.Linear(hidden_dim, num_classes)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        self.segmentation_head = segmentation_head
+
+        query_dim=4
+        self.refpoint_embed = nn.Embedding(num_queries * group_detr, query_dim)
+        self.query_feat = nn.Embedding(num_queries * group_detr, hidden_dim)
+        nn.init.constant_(self.refpoint_embed.weight.data, 0)
+
+        self.backbone = backbone
+        self.aux_loss = aux_loss
+        self.group_detr = group_detr
+
+        # iter update
+        self.lite_refpoint_refine = lite_refpoint_refine
+        if not self.lite_refpoint_refine:
+            self.transformer.decoder.bbox_embed = self.bbox_embed
+        else:
+            self.transformer.decoder.bbox_embed = None
+
+        self.bbox_reparam = bbox_reparam
+
+        # init prior_prob setting for focal loss
+        prior_prob = 0.01
+        bias_value = -math.log((1 - prior_prob) / prior_prob)
+        self.class_embed.bias.data = torch.ones(num_classes) * bias_value
+
+        # init bbox_mebed
+        nn.init.constant_(self.bbox_embed.layers[-1].weight.data, 0)
+        nn.init.constant_(self.bbox_embed.layers[-1].bias.data, 0)
+
+        # two_stage
+        self.two_stage = two_stage
+        if self.two_stage:
+            self.transformer.enc_out_bbox_embed = nn.ModuleList(
+                [copy.deepcopy(self.bbox_embed) for _ in range(group_detr)])
+            self.transformer.enc_out_class_embed = nn.ModuleList(
+                [copy.deepcopy(self.class_embed) for _ in range(group_detr)])
+
+        self._export = False
+
+    def reinitialize_detection_head(self, num_classes):
+        base = self.class_embed.weight.shape[0]
+        num_repeats = int(math.ceil(num_classes / base))
+        self.class_embed.weight.data = self.class_embed.weight.data.repeat(num_repeats, 1)
+        self.class_embed.weight.data = self.class_embed.weight.data[:num_classes]
+        self.class_embed.bias.data = self.class_embed.bias.data.repeat(num_repeats)
+        self.class_embed.bias.data = self.class_embed.bias.data[:num_classes]
+
+        if self.two_stage:
+            for enc_out_class_embed in self.transformer.enc_out_class_embed:
+                enc_out_class_embed.weight.data = enc_out_class_embed.weight.data.repeat(num_repeats, 1)
+                enc_out_class_embed.weight.data = enc_out_class_embed.weight.data[:num_classes]
+                enc_out_class_embed.bias.data = enc_out_class_embed.bias.data.repeat(num_repeats)
+                enc_out_class_embed.bias.data = enc_out_class_embed.bias.data[:num_classes]
+
+    def export(self):
+        self._export = True
+        self._forward_origin = self.forward
+        self.forward = self.forward_export
+        for name, m in self.named_modules():
+            if hasattr(m, "export") and isinstance(m.export, Callable) and hasattr(m, "_export") and not m._export:
+                m.export()
+
+    def forward(self, samples: NestedTensor, targets=None):
+        """The forward expects a NestedTensor, which consists of:
+               - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
+               - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
+
+            It returns a dict with the following elements:
+               - "pred_logits": the classification logits (including no-object) for all queries.
+                                Shape= [batch_size x num_queries x num_classes]
+               - "pred_boxes": The normalized boxes coordinates for all queries, represented as
+                               (center_x, center_y, width, height). These values are normalized in [0, 1],
+                               relative to the size of each individual image (disregarding possible padding).
+                               See PostProcess for information on how to retrieve the unnormalized bounding box.
+               - "aux_outputs": Optional, only returned when auxilary losses are activated. It is a list of
+                                dictionnaries containing the two above keys for each decoder layer.
+        """
+        if isinstance(samples, (list, torch.Tensor)):
+            samples = nested_tensor_from_tensor_list(samples)
+        features, poss = self.backbone(samples)
+
+        srcs = []
+        masks = []
+        for l, feat in enumerate(features):
+            src, mask = feat.decompose()
+            srcs.append(src)
+            masks.append(mask)
+            assert mask is not None
+
+        if self.training:
+            refpoint_embed_weight = self.refpoint_embed.weight
+            query_feat_weight = self.query_feat.weight
+        else:
+            # only use one group in inference
+            refpoint_embed_weight = self.refpoint_embed.weight[:self.num_queries]
+            query_feat_weight = self.query_feat.weight[:self.num_queries]
+
+        if self.segmentation_head is not None:
+            seg_head_fwd = self.segmentation_head.sparse_forward if self.training else self.segmentation_head.forward
+
+        hs, ref_unsigmoid, hs_enc, ref_enc = self.transformer(
+            srcs, masks, poss, refpoint_embed_weight, query_feat_weight)
+
+        if hs is not None:
+            if self.bbox_reparam:
+                outputs_coord_delta = self.bbox_embed(hs)
+                outputs_coord_cxcy = outputs_coord_delta[..., :2] * ref_unsigmoid[..., 2:] + ref_unsigmoid[..., :2]
+                outputs_coord_wh = outputs_coord_delta[..., 2:].exp() * ref_unsigmoid[..., 2:]
+                outputs_coord = torch.concat(
+                    [outputs_coord_cxcy, outputs_coord_wh], dim=-1
+                )
+            else:
+                outputs_coord = (self.bbox_embed(hs) + ref_unsigmoid).sigmoid()
+
+            outputs_class = self.class_embed(hs)
+
+            if self.segmentation_head is not None:
+                outputs_masks = seg_head_fwd(features[0].tensors, hs, samples.tensors.shape[-2:])
+
+            out = {'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]}
+            if self.segmentation_head is not None:
+                out['pred_masks'] = outputs_masks[-1]
+            if self.aux_loss:
+                out['aux_outputs'] = self._set_aux_loss(outputs_class, outputs_coord, outputs_masks if self.segmentation_head is not None else None)
+
+        if self.two_stage:
+            group_detr = self.group_detr if self.training else 1
+            hs_enc_list = hs_enc.chunk(group_detr, dim=1)
+            cls_enc = []
+            for g_idx in range(group_detr):
+                cls_enc_gidx = self.transformer.enc_out_class_embed[g_idx](hs_enc_list[g_idx])
+                cls_enc.append(cls_enc_gidx)
+
+            cls_enc = torch.cat(cls_enc, dim=1)
+
+            if self.segmentation_head is not None:
+                masks_enc = seg_head_fwd(features[0].tensors, [hs_enc,], samples.tensors.shape[-2:], skip_blocks=True)[0]
+
+            if hs is not None:
+                out['enc_outputs'] = {'pred_logits': cls_enc, 'pred_boxes': ref_enc}
+                if self.segmentation_head is not None:
+                    out['enc_outputs']['pred_masks'] = masks_enc
+            else:
+                out = {'pred_logits': cls_enc, 'pred_boxes': ref_enc}
+                if self.segmentation_head is not None:
+                    out['pred_masks'] = masks_enc
+
+        return out
+
+    def forward_export(self, tensors):
+        srcs, _, poss = self.backbone(tensors)
+        # only use one group in inference
+        refpoint_embed_weight = self.refpoint_embed.weight[:self.num_queries]
+        query_feat_weight = self.query_feat.weight[:self.num_queries]
+
+        hs, ref_unsigmoid, hs_enc, ref_enc = self.transformer(
+            srcs, None, poss, refpoint_embed_weight, query_feat_weight)
+
+        outputs_masks = None
+
+        if hs is not None:
+            if self.bbox_reparam:
+                outputs_coord_delta = self.bbox_embed(hs)
+                outputs_coord_cxcy = outputs_coord_delta[..., :2] * ref_unsigmoid[..., 2:] + ref_unsigmoid[..., :2]
+                outputs_coord_wh = outputs_coord_delta[..., 2:].exp() * ref_unsigmoid[..., 2:]
+                outputs_coord = torch.concat(
+                    [outputs_coord_cxcy, outputs_coord_wh], dim=-1
+                )
+            else:
+                outputs_coord = (self.bbox_embed(hs) + ref_unsigmoid).sigmoid()
+            outputs_class = self.class_embed(hs)
+            if self.segmentation_head is not None:
+                outputs_masks = self.segmentation_head(srcs[0], [hs,], tensors.shape[-2:])[0]
+        else:
+            assert self.two_stage, "if not using decoder, two_stage must be True"
+            outputs_class = self.transformer.enc_out_class_embed[0](hs_enc)
+            outputs_coord = ref_enc
+            if self.segmentation_head is not None:
+                outputs_masks = self.segmentation_head(srcs[0], [hs_enc,], tensors.shape[-2:], skip_blocks=True)[0]
+
+        if outputs_masks is not None:
+            return outputs_coord, outputs_class, outputs_masks
+        else:
+            return outputs_coord, outputs_class
+
+    @torch.jit.unused
+    def _set_aux_loss(self, outputs_class, outputs_coord, outputs_masks):
+        # this is a workaround to make torchscript happy, as torchscript
+        # doesn't support dictionary with non-homogeneous values, such
+        # as a dict having both a Tensor and a list.
+        if outputs_masks is not None:
+            return [{'pred_logits': a, 'pred_boxes': b, 'pred_masks': c}
+                    for a, b, c in zip(outputs_class[:-1], outputs_coord[:-1], outputs_masks[:-1])]
+        else:
+            return [{'pred_logits': a, 'pred_boxes': b}
+                    for a, b in zip(outputs_class[:-1], outputs_coord[:-1])]
+
+    def update_drop_path(self, drop_path_rate, vit_encoder_num_layers):
+        """ """
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, vit_encoder_num_layers)]
+        for i in range(vit_encoder_num_layers):
+            if hasattr(self.backbone[0].encoder, 'blocks'): # Not aimv2
+                if hasattr(self.backbone[0].encoder.blocks[i].drop_path, 'drop_prob'):
+                    self.backbone[0].encoder.blocks[i].drop_path.drop_prob = dp_rates[i]
+            else: # aimv2
+                if hasattr(self.backbone[0].encoder.trunk.blocks[i].drop_path, 'drop_prob'):
+                    self.backbone[0].encoder.trunk.blocks[i].drop_path.drop_prob = dp_rates[i]
+
+    def update_dropout(self, drop_rate):
+        for module in self.transformer.modules():
+            if isinstance(module, nn.Dropout):
+                module.p = drop_rate
 
 def build_model(args):
     # the `num_classes` naming here is somewhat misleading.
@@ -516,7 +776,7 @@ class RFDETR:
                 img = Image.open(img)
 
             if not isinstance(img, torch.Tensor):
-                img = F.to_tensor(img)
+                img = vF.to_tensor(img)
 
             if (img > 1).any():
                 raise ValueError(
@@ -534,8 +794,8 @@ class RFDETR:
             orig_sizes.append((h, w))
 
             img_tensor = img_tensor.to(self.model.device)
-            img_tensor = F.normalize(img_tensor, self.means, self.stds)
-            img_tensor = F.resize(img_tensor, (self.model.resolution, self.model.resolution))
+            img_tensor = vF.normalize(img_tensor, self.means, self.stds)
+            img_tensor = vF.resize(img_tensor, (self.model.resolution, self.model.resolution))
 
             processed_images.append(img_tensor)
 
