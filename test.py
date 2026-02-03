88fc5e994c52ddba566fcfb702cb20e31cd5870c
roryclear <roryclear.rc@gmail.com>
2026-02-03 16:11:15 +0000

    move

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import supervision as sv
 from PIL import Image
 import numpy as np
 from collections import defaultdict
-from rfdetr.models.backbone.dinov2 import DinoV2
-from rfdetr.models.ops.functions import ms_deform_attn_core_pytorch
+from rfdetr.models.backbone.dinov2 import get_config, WindowedDinov2WithRegistersConfig, WindowedDinov2WithRegistersBackbone, size_to_width
 
 import torch
 from torch import nn
@@ -64,6 +63,174 @@ OPEN_SOURCE_MODELS = {
 }
 
 HOSTED_MODELS = {**OPEN_SOURCE_MODELS, **PLATFORM_MODELS}
+
+class DinoV2(nn.Module):
+    def __init__(self,
+            shape=(640, 640),
+            out_feature_indexes=[2, 4, 5, 9],
+            size="base",
+            use_registers=True,
+            use_windowed_attn=True,
+            gradient_checkpointing=False,
+            load_dinov2_weights=True,
+            patch_size=14,
+            num_windows=4,
+            positional_encoding_size=37,
+            ):
+        super().__init__()
+
+        name = f"facebook/dinov2-with-registers-{size}" if use_registers else f"facebook/dinov2-{size}"
+
+        self.shape = shape
+        self.patch_size = patch_size
+        self.num_windows = num_windows
+
+        # Create the encoder
+
+        if not use_windowed_attn:
+            assert not gradient_checkpointing, "Gradient checkpointing is not supported for non-windowed attention"
+            assert load_dinov2_weights, "Using non-windowed attention requires loading dinov2 weights from hub"
+            self.encoder = AutoBackbone.from_pretrained(
+                name,
+                out_features=[f"stage{i}" for i in out_feature_indexes],
+                return_dict=False,
+            )
+        else:
+            window_block_indexes = set(range(out_feature_indexes[-1] + 1))
+            window_block_indexes.difference_update(out_feature_indexes)
+            window_block_indexes = list(window_block_indexes)
+
+            dino_config = get_config(size, use_registers)
+
+            dino_config["return_dict"] = False
+            dino_config["out_features"] = [f"stage{i}" for i in out_feature_indexes]
+
+            implied_resolution = positional_encoding_size * patch_size
+
+            if implied_resolution != dino_config["image_size"]:
+                print("Using a different number of positional encodings than DINOv2, which means we're not loading DINOv2 backbone weights. This is not a problem if finetuning a pretrained RF-DETR model.")
+                dino_config["image_size"] = implied_resolution
+                load_dinov2_weights = False
+
+            if patch_size != 14:
+                print(f"Using patch size {patch_size} instead of 14, which means we're not loading DINOv2 backbone weights. This is not a problem if finetuning a pretrained RF-DETR model.")
+                dino_config["patch_size"] = patch_size
+                load_dinov2_weights = False
+
+            if use_registers:
+                windowed_dino_config = WindowedDinov2WithRegistersConfig(
+                    **dino_config,
+                    num_windows=num_windows,
+                    window_block_indexes=window_block_indexes,
+                    gradient_checkpointing=gradient_checkpointing,
+                )
+            else:
+                windowed_dino_config = WindowedDinov2WithRegistersConfig(
+                    **dino_config,
+                    num_windows=num_windows,
+                    window_block_indexes=window_block_indexes,
+                    num_register_tokens=0,
+                    gradient_checkpointing=gradient_checkpointing,
+                )
+            self.encoder = WindowedDinov2WithRegistersBackbone.from_pretrained(
+                name,
+                config=windowed_dino_config,
+            ) if load_dinov2_weights else WindowedDinov2WithRegistersBackbone(windowed_dino_config)
+
+
+        self._out_feature_channels = [size_to_width[size]] * len(out_feature_indexes)
+        self._export = False
+
+    def export(self):
+        if self._export:
+            return
+        self._export = True
+        shape = self.shape
+        def make_new_interpolated_pos_encoding(
+            position_embeddings, patch_size, height, width
+        ):
+
+            num_positions = position_embeddings.shape[1] - 1
+            dim = position_embeddings.shape[-1]
+            height = height // patch_size
+            width = width // patch_size
+
+            class_pos_embed = position_embeddings[:, 0]
+            patch_pos_embed = position_embeddings[:, 1:]
+
+            # Reshape and permute
+            patch_pos_embed = patch_pos_embed.reshape(
+                1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim
+            )
+            patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
+
+            # Use bilinear interpolation without antialias
+            patch_pos_embed = F.interpolate(
+                patch_pos_embed,
+                size=(height, width),
+                mode="bicubic",
+                align_corners=False,
+                antialias=True,
+            )
+
+            # Reshape back
+            patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).reshape(1, -1, dim)
+            return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+
+        # If the shape of self.encoder.embeddings.position_embeddings
+        # matches the shape of your new tensor, use copy_:
+        with torch.no_grad():
+            new_positions = make_new_interpolated_pos_encoding(
+                self.encoder.embeddings.position_embeddings,
+                self.encoder.config.patch_size,
+                shape[0],
+                shape[1],
+            )
+        # Create a new Parameter with the new size
+        old_interpolate_pos_encoding = self.encoder.embeddings.interpolate_pos_encoding
+        def new_interpolate_pos_encoding(self_mod, embeddings, height, width):
+            num_patches = embeddings.shape[1] - 1
+            num_positions = self_mod.position_embeddings.shape[1] - 1
+            if num_patches == num_positions and height == width:
+                return self_mod.position_embeddings
+            return old_interpolate_pos_encoding(embeddings, height, width)
+
+        self.encoder.embeddings.position_embeddings = nn.Parameter(new_positions)
+        self.encoder.embeddings.interpolate_pos_encoding = types.MethodType(
+            new_interpolate_pos_encoding,
+            self.encoder.embeddings
+        )
+
+    def forward(self, x):
+        block_size = self.patch_size * self.num_windows
+        assert x.shape[2] % block_size == 0 and x.shape[3] % block_size == 0, f"Backbone requires input shape to be divisible by {block_size}, but got {x.shape}"
+        x = self.encoder(x)
+        return list(x[0])
+
+def ms_deform_attn_core_pytorch(value, value_spatial_shapes, sampling_locations, attention_weights):
+    """"for debug and test only, need to use cuda version instead
+    """
+    # B, n_heads, head_dim, N
+    B, n_heads, head_dim, _ = value.shape
+    _, Len_q, n_heads, L, P, _ = sampling_locations.shape
+    value_list = value.split([H * W for H, W in value_spatial_shapes], dim=3)
+    sampling_grids = 2 * sampling_locations - 1
+    sampling_value_list = []
+    for lid_, (H, W) in enumerate(value_spatial_shapes):
+        # B, n_heads, head_dim, H, W
+        value_l_ = value_list[lid_].view(B * n_heads, head_dim, H, W)
+        # B, Len_q, n_heads, P, 2 -> B, n_heads, Len_q, P, 2 -> B*n_heads, Len_q, P, 2
+        sampling_grid_l_ = sampling_grids[:, :, :, lid_].transpose(1, 2).flatten(0, 1)
+        # B*n_heads, head_dim, Len_q, P
+        sampling_value_l_ = F.grid_sample(value_l_, sampling_grid_l_,
+                                          mode='bilinear', padding_mode='zeros', align_corners=False)
+        sampling_value_list.append(sampling_value_l_)
+    # (B, Len_q, n_heads, L * P) -> (B, n_heads, Len_q, L, P) -> (B*n_heads, 1, Len_q, L*P)
+    attention_weights = attention_weights.transpose(1, 2).reshape(B * n_heads, 1, Len_q, L * P)
+    # B*n_heads, head_dim, Len_q, L*P
+    sampling_value_list = torch.stack(sampling_value_list, dim=-2).flatten(-2)
+    output = (sampling_value_list * attention_weights).sum(-1).view(B, n_heads * head_dim, Len_q)
+    return output.transpose(1, 2).contiguous()
 
 def _get_activation_fn(activation):
     """Return an activation function given a string"""
